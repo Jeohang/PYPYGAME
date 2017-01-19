0dc08e783b87b8cc649463d6c387132d6e3a0078
jjunny <jjunyoung99@gmail.com>
2017-01-19 22:40:45 +0900

    Delete the class and rename the variable.

---
+++ Map.py
@@ -0,0 +1,25 @@
+import pygame as pg
+import Action
+import SQW
+pg.init()
+
+display_width = 800
+display_height = 450
+ourScreen = pg.display.set_mode((display_width, display_height))
+rect_x = 80
+rect_y = 80
+arrow_r = pg.image.load("arrows\\arrow_r.jpg")
+arrow_l = pg.image.load("arrows\\arrow_l.jpg")
+arrow_d = pg.image.load("arrows\\arrow_d.jpg")
+arrow_u = pg.image.load("arrows\\arrow_u.jpg")
+straight = pg.image.load("button\\straight.jpg")
+left = pg.image.load("button\\left.jpg")
+right = pg.image.load("button\\right.jpg")
+action = pg.image.load("button\\action.jpg")
+clear = pg.image.load("button\\clear.jpg")
+run = pg.image.load("button\\run.jpg")
+setting = pg.image.load("button\\setting.jpg")
+option = pg.image.load('button\\option.jpg')
+map1 = pg.image.load("maps\\map1.jpg")
+a = 20
+b = 25
--- SQW.py
+++ SQW.py
@@ -1,20 +1,20 @@
 import pygame as pg
-import SQW_Action
-import SQW_Map
+import Action
+import Map
 pg.init()
 point = 80
 
 
-def run_TT():
-    if SQW_Action.Action.status[0] == 1:
-        for choice in SQW_Action.Action.TT:
+def run_execution_list():
+    if Action.status[0] == 1:
+        for choice in Action.execution_list:
             if choice == "1":
-                SQW_Action.Action.moving_arrow_r(100, 25)  # 좌표가 문제
+                Action.moving_arrow_r(100, 25)  # 좌표가 문제
                 print(type(choice))
             elif choice == "2":
-                SQW_Action.Action.moving_arrow_u(20, 25)
+                Action.moving_arrow_u(20, 25)
             elif choice == "3":
-                SQW_Action.Action.moving_arrow_d(20, 25)
+                Action.moving_arrow_d(20, 25)
             else:
                 pass
 
@@ -31,22 +31,22 @@ def __main__():
                     for y in range(35, 90):
                         if (event.pos[0], event.pos[1]) == (x, y):
                             print("go straight")
-                            SQW_Action.Action.TT.append("1")
+                            Action.execution_list.append("1")
                 for x in range(630, 690):
                     for y in range(35, 90):
                         if (event.pos[0], event.pos[1]) == (x, y):
                             print("turn left")
-                            SQW_Action.Action.TT.append(2)
+                            Action.execution_list.append(2)
                 for x in range(705, 765):
                     for y in range(35, 90):
                         if (event.pos[0], event.pos[1]) == (x, y):
                             print("turn right")
-                            SQW_Action.Action.TT.append(3)
+                            Action.execution_list.append(3)
                 for x in range(555, 615):
                     for y in range(110, 165):
                         if (event.pos[0], event.pos[1]) == (x, y):
                             print("action")
-                            SQW_Action.Action.TT.append(4)
+                            Action.execution_list.append(4)
                 for x in range(630, 690):
                     for y in range(110, 165):
                         if (event.pos[0], event.pos[1]) == (x, y):
@@ -63,20 +63,21 @@ def __main__():
                     for y in range(180, 200):
                         if (event.pos[0], event.pos[1]) == (x, y):
                             print("setting")
-        SQW_Map.Map.ourScreen.fill((0, 0, 0))
-        SQW_Map.Map.ourScreen.blit(SQW_Map.Map.map, (20, 20))
-        pg.draw.rect(SQW_Map.Map.ourScreen, (0, 255, 0), pg.Rect(540, 20, 240, 190))  # button part
-        SQW_Action.Action.moving_arrow_r(20, 25)
-        SQW_Action.Action.straight(555, 35)
-        SQW_Action.Action.left(630, 35)
-        SQW_Action.Action.right(705, 35)
-        SQW_Action.Action.action(555, 110)
-        SQW_Action.Action.clear(630, 110)
-        SQW_Action.Action.run(705, 110)
-        SQW_Action.Action.option(555, 180)
-        SQW_Action.Action.setting(665, 180)
-        SQW_Action.Action.moving_arrow_r(SQW_Map.Map.a, SQW_Map.Map.b)
-        pg.draw.rect(SQW_Map.Map.ourScreen, (0, 255, 255), pg.Rect(540, 230, 240, 200))  # function part
+        Map.ourScreen.fill((0, 0, 0))
+        # print(Action.execution_list)
+        Map.ourScreen.blit(Map.map1, (20, 20))
+        pg.draw.rect(Map.ourScreen, (0, 255, 0), pg.Rect(540, 20, 240, 190))  # button part
+        Action.moving_arrow_r(20, 25)
+        Action.straight(555, 35)
+        Action.left(630, 35)
+        Action.right(705, 35)
+        Action.action(555, 110)
+        Action.clear(630, 110)
+        Action.run(705, 110)
+        Action.option(555, 180)
+        Action.setting(665, 180)
+        Action.moving_arrow_r(Map.a, Map.b)
+        pg.draw.rect(Map.ourScreen, (0, 255, 255), pg.Rect(540, 230, 240, 200))  # function part
         pg.display.flip()
         clock.tick(60)
 
